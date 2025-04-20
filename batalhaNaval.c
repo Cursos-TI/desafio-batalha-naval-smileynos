11f9573a77631bb8529a088b3d343dbbe02b1f90
smileynos <israelsantanna2@outlook.com>
2025-04-20 20:07:47 +0000

    Novo commit

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,138 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define LINHAS 10
+#define COLUNAS 10
+#define NAVIO 3
+#define MAR 0
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Declaração de variáveis
+    int linha[LINHAS];
+    char coluna[COLUNAS];
+    int tabuleiro[LINHAS][COLUNAS];
+    int navioHorizontal[NAVIO];
+    int navioVertical[NAVIO];
+    int navioDiagonalPri[NAVIO];
+    int navioDiagonalSec[NAVIO];
+    int habcone [NAVIO];
+    int habcruz [NAVIO];
+    int haboctaedro [NAVIO];
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    //  tabuleiro com o valor 0
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            tabuleiro[i][j] = MAR;
+        }
+    }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    for (int i = 0; i < COLUNAS; i++){
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+        coluna[i] = 'A' + i;
+    }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+  
+    for (int i =0; i < LINHAS; i++){
+        linha[i] = i+1;
+    }
+    // Posiçã do navio na horizontal
+    int coordenadHorzintal = 2;
+    int coordenadaVertical = 3;
+    for (int i = 0; i < 2; i++){
+        navioHorizontal[i] = NAVIO;
+        tabuleiro[coordenadHorzintal ][coordenadaVertical] = navioHorizontal [i];
+        coordenadaVertical++;
+    } 
+    // Posição do Navio na Vertical
+  
+    int coordenadaHorizontal2 = 5;
+    int coordenadaVertical2 = 7;
+    for (int j = 0; j < NAVIO; j++){
+        navioVertical [j] = NAVIO;
+        tabuleiro [coordenadaHorizontal2] [coordenadaVertical2] = navioVertical [j];
+        coordenadaHorizontal2++;
+    }
+    // Posição do Navio na DiagonalPrincipal
+    int Diagonalprincipal = 1;
+    for (int i = 0; i < NAVIO; i++){
+        navioDiagonalPri[i] = NAVIO;
+        for (int j = 0; j < NAVIO; j++){
+            tabuleiro[Diagonalprincipal][Diagonalprincipal] = navioDiagonalPri[i];
+            
+        }
+        Diagonalprincipal ++;
+    } 
 
+    // Navio DiagonalSecundaria
+    int diagonalSecundaria = 0; // Começa na linha 0
+    for (int i = 0; i < NAVIO; i++) {
+        navioDiagonalSec[i] = NAVIO;
+        tabuleiro[diagonalSecundaria][9 - diagonalSecundaria] = navioDiagonalSec[i]; 
+        diagonalSecundaria++;
+    }
+    // Habilidade Cone
+    int coordenadHabconeH= 3; //posição na horizontal
+    int coordenadaHabconeV = 3; //posição na na Vertical
+    for (int i = 0; i < 5; i++){
+        navioHorizontal[i] = NAVIO;
+        tabuleiro[coordenadHabconeH ][coordenadaHabconeV - 2] = navioHorizontal [i];
+        coordenadaHabconeV++;
+    } 
+    int coordenadaHabconeH2 = 1; //posição na horizontal
+    int coordenadaHabconeV2 = 3; //posição na Vertical
+    for (int j = 0; j < 2; j++){
+    habcone [j] = NAVIO;
+    tabuleiro [coordenadaHabconeH2 ] [coordenadaHabconeV2] = habcone [j];
+    coordenadaHabconeH2++;
+    }
+
+    // Habilidade em Cruz
+    int habcruzhorizontal = 5; //posição na horizontal
+    int habcruzvertical = 6; //posição na Vertical
+    for (int i = 0; i < 5; i++){
+        navioHorizontal[i] = NAVIO;
+        tabuleiro[habcruzhorizontal + 1][habcruzvertical - 1] = navioHorizontal [i];
+        habcruzvertical++;
+    } 
+    // Habilidade em octoedro
+    int coordenadHaboctoedroH1 = 5; //posição na horizontal
+    int coordenadaHabctoedroV1 = 6; //posição na Vertical
+    for (int i = 0; i <NAVIO; i++){
+        navioHorizontal[i] = NAVIO;
+        tabuleiro[coordenadHaboctoedroH1 + 1][coordenadaHabctoedroV1- 5] = navioHorizontal [i];
+        coordenadaHabctoedroV1++;
+    } 
+
+    int coordenadaHaboctoedroH2 = 5;
+    int coordenadaHaboctoedroV2 = 7;
+    for(int i = 0; i <2; i++){
+        haboctaedro [i] = NAVIO;
+        tabuleiro [coordenadaHaboctoedroH2 + 1] [coordenadaHaboctoedroV2 - 5] = haboctaedro [i];
+        coordenadaHaboctoedroH2--;
+        for (int j = 0; j < 2; j++){
+        habcone [j] = NAVIO;
+        tabuleiro [coordenadaHaboctoedroH2 + 1] [coordenadaHaboctoedroV2 - 5] = haboctaedro [j];
+        coordenadaHaboctoedroH2++;
+        }
+    }
+       
+    // Exibe o nome do jogo
+    printf("TABULEIRO BATALHA-NAVAL\n");
+    printf("   ");
+ 
+    // Imprime na tela as colunas representada pelas letras de A á J 
+    for (int i = 0; i < COLUNAS; i++) {
+    printf("%c ", coluna[i]);
+    }
+    printf("\n");
+ 
+    // Exibe a posição dos navios no tabuleiro
+    for (int i = 0; i < LINHAS; i++) {
+    printf("%2d", linha[i]);
+    for (int j = 0; j < COLUNAS; j++) {
+        printf("%2d", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
     return 0;
 }
